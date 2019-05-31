e1dba6e1f789a646a276661e9de26a8a8ab7cbf0
Ben Podgursky <bpodgursky@gmail.com>
2019-05-31 12:08:30 -0700

    push these images to dockerhub too

--- workflow_db/bin/publish-containers.sh
+++ workflow_db/bin/publish-containers.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+MIGRATION_REPO=liveramp/workflow2_db_migrations
 docker build -t db_migrations:latest -f container/migration/Dockerfile .
-docker tag db_migrations gcr.io/liveramp-eng/workflow2/db_migrations
-docker push gcr.io/liveramp-eng/workflow2/db_migrations
+docker tag db_migrations ${MIGRATION_REPO}
+docker push ${MIGRATION_REPO}
 
+SQLDUMP_REPO=liveramp/workflow2_sqldump
 docker build -t sqldump:latest -f container/sqldump/Dockerfile .
-docker tag sqldump gcr.io/liveramp-eng/workflow2/sqldump
-docker push gcr.io/liveramp-eng/workflow2/sqldump
+docker tag sqldump ${SQLDUMP_REPO}
+docker push ${SQLDUMP_REPO}
