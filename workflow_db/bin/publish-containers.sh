05f3da8d925aaaa439bdd23c431fd34871c7d3f2
Ben Podgursky <bpodgursky@gmail.com>
2019-05-31 12:15:17 -0700

    try to fix push

--- workflow_db/bin/publish-containers.sh
+++ workflow_db/bin/publish-containers.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+docker_login() {
+    local restore_x
+    if [[ $- =~ x ]]; then
+      restore_x=true
+    else
+      restore_x=false
+    fi
+
+    # Disable xtrace to hide password ($DOCKER_PASS).
+    set +x
+    echo "${DOCKER_PASS}" | docker login -u ${DOCKER_USER} --password-stdin
+    if [[ ${restore_x} ]]; then
+        set -x
+    fi
+}
+
+docker_login
+
 MIGRATION_REPO=liveramp/workflow2_db_migrations
 docker build -t db_migrations:latest -f container/migration/Dockerfile .
 docker tag db_migrations ${MIGRATION_REPO}
