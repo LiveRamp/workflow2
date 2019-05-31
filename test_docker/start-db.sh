5e315341920bbcaa849780b189bd06f76d3ebcda
Ben Podgursky <bpodgursky@gmail.com>
2019-05-31 13:35:58 -0700

    use new test image

--- test_docker/start-db.sh
+++ test_docker/start-db.sh
@@ -5,5 +5,5 @@ DIR="${0%/*}"
 
 $DIR/start-mysql.sh
 
-docker pull gcr.io/liveramp-eng/workflow2/sqldump:latest
-docker run --rm --name test_migrate --network=workflow_test_shared -e "DB_PORT=3306" -e "DB_USERNAME=root" -e "DB_HOSTNAME=mysql" gcr.io/liveramp-eng/workflow2/sqldump:latest
+docker pull liveramp/workflow2_sqldump:latest
+docker run --rm --name test_migrate --network=workflow_test_shared -e "DB_PORT=3306" -e "DB_USERNAME=root" -e "DB_HOSTNAME=mysql" liveramp/workflow2_sqldump:latest
