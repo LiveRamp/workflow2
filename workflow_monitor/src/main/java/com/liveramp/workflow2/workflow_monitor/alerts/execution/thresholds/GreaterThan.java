35095a33498f77d37617a2192ee6ec24816eb422
Ben Podgursky <bpodgursky@gmail.com>
2019-06-02 20:19:34 -0700

    Revert "repackage"
    This reverts commit b42d964fccbf4e8a67273e4462998d001d6b981f.

--- workflow_monitor/src/main/java/com/liveramp/workflow2/workflow_monitor/alerts/execution/thresholds/GreaterThan.java
+++
@@ -1,7 +0,0 @@
-package com.liveramp.workflow2.workflow_monitor.alerts.execution.thresholds;
-
-public class GreaterThan implements ThresholdChecker {
-  public boolean isAlert(Double threshold, Double value) {
-    return value > threshold;
-  }
-}
--- workflow_monitor/src/main/java/com/liveramp/workflow2/workflow_monitor/alerts/execution/thresholds/ThresholdChecker.java
+++
@@ -1,5 +0,0 @@
-package com.liveramp.workflow2.workflow_monitor.alerts.execution.thresholds;
-
-public interface ThresholdChecker {
-  boolean isAlert(Double threshold, Double value);
-}
